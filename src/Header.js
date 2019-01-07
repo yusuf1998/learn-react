83d39e56339aa747732c2d77548bacf3dc6d6d1d
yusuf1998 <yusufhabibi1998@gmail.com>
2019-01-07 15:21:04 +0700

    First Commit

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Yusuf Habibi</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
